2f00c39c702f6192d4c04ac22ff92f0b67e1ca67
Rahul Beniwal <rahul.1802305029@sanskriti.ac.in>
2020-11-20 00:02:19 +0530

    declaring commit , finishing touch

---
+++ scripts/js/index.js
@@ -0,0 +1,7 @@
+var penPostModal = document.getElementById("penPostModal");
+var createPostButton = document.getElementById("button2");
+var textarea = document.getElementById("Content");
+
+/* almost all functionlity of modal is defiened in header.js for make code short and 
+due to code reusbality ,  only variable are defined here */
+
