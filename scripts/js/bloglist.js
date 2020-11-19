085bec6bd6fc420b821a7b801a2939348a0db41d
Rahul Beniwal <rahul.1802305029@sanskriti.ac.in>
2020-11-19 22:41:11 +0530

    adding some documentation

--- scripts/js/bloglist.js
+++ scripts/js/bloglist.js
@@ -15,6 +15,7 @@ function showDeleteModal() {
     model.style.display = "block";
     console.log("clicked");
 }
+
 hideDeleteModal.addEventListener("click", hideModal);
 function hideModal() {
     try {
@@ -25,3 +26,70 @@ function hideModal() {
     }
     console.log("close clicked");
 }
+
+var buttonClicked = {};
+for (var i = 0; i <= 4; i++) {
+    buttonClicked[`button${i}`] = document.getElementById(`more${i + 1}`);
+}
+
+var j = 0;
+function fetchData1() {
+    index = 1;
+    var posttitle = document.getElementById(`title` + index).innerHTML;
+    var authorName = document.getElementById(`authorname` + index).innerHTML;
+    var postContent = document.getElementById(`postcontent` + index).innerHTML;
+    document.cookie = `authorname=${authorName}; expires=Thu, 18 Dec 2021 12:00:00 UTC`;
+    localStorage.setItem("title", posttitle);
+    localStorage.setItem("postcontent", postContent);
+
+}
+function fetchData2() {
+    var index = 2;
+    var posttitle = document.getElementById(`title` + index).innerHTML;
+    var authorName = document.getElementById(`authorname` + index).innerHTML;
+    var postContent = document.getElementById(`postcontent` + index).innerHTML;
+    document.cookie = `authorname=${authorName}; expires=Thu, 18 Dec 2021 12:00:00 UTC`;
+    localStorage.setItem("postcontent", postContent);
+    localStorage.setItem("title", posttitle);
+
+
+}
+function fetchData3() {
+    var index = 3;
+    var posttitle = document.getElementById(`title` + index).innerHTML;
+    var authorName = document.getElementById(`authorname` + index).innerHTML;
+    var postContent = document.getElementById(`postcontent` + index).innerHTML;
+    console.log(authorName, postContent);
+    document.cookie = `authorname=${authorName}; expires=Thu, 18 Dec 2021 12:00:00 UTC`;
+    localStorage.setItem("postcontent", postContent);
+    localStorage.setItem("title", posttitle);
+
+
+}
+function fetchData4() {
+    var index = 4;
+    var authorName = document.getElementById(`authorname` + index).innerHTML;
+    var postContent = document.getElementById(`postcontent` + index).innerHTML;
+    document.cookie = `authorname=${authorName}; expires=Thu, 18 Dec 2021 12:00:00 UTC`;
+    localStorage.setItem("postcontent", postContent);
+    localStorage.setItem("title", posttitle);
+
+
+
+}
+function fetchData5() {
+    let index = 5;
+    var posttitle = document.getElementById(`title` + index).innerHTML;
+    var authorName = document.getElementById(`authorname` + index).innerHTML;
+    var postContent = document.getElementById(`postcontent` + index).innerHTML;
+    document.cookie = `authorname=${authorName}; expires=Thu, 18 Dec 2021 12:00:00 UTC`;
+    localStorage.setItem("postcontent", postContent);
+    localStorage.setItem("title", posttitle);
+
+
+}
+buttonClicked.button0.addEventListener("click", fetchData1);
+buttonClicked.button1.addEventListener("click", fetchData2);
+buttonClicked.button2.addEventListener("click", fetchData3);
+buttonClicked.button3.addEventListener("click", fetchData4);
+buttonClicked.button4.addEventListener("click", fetchData5);
